3d5fa173d472ed5f0b4d898a73c3c917c9c2bc80
chintam anand <chintamanand56@gmail.com>
2018-04-15 23:37:39 +0530

    lab-7 Work

---
+++ lab-7/example1.d
@@ -0,0 +1,8 @@
+example1.o: example1.c
+example1.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+example1.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+example1.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+example1.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+example1.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+example1.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
+example1.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
